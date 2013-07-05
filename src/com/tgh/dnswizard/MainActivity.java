9f10cf1fcb657b5f4c927a51f2fcf370b69ad7a8
Albert.Tian <emellend@gmail.com>
2013-07-05 11:37:40 +0800

    初始化项目框架

---
+++ src/com/tgh/dnswizard/MainActivity.java
@@ -0,0 +1,18 @@
+package com.tgh.dnswizard;
+
+import android.os.Bundle;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: emellend
+ * Date: 13-7-5
+ * Time: 上午11:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainActivity extends SherlockFragmentActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
